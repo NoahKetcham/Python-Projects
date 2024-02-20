8d15eabff822cffb0528f9b3a09c9cd21b098433
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2024-02-20 10:49:48 -0800

    Update file_transfer

--- Assignments/file_transfer.py
+++ Assignments/file_transfer.py
@@ -3,6 +3,8 @@ from tkinter import *
 import tkinter.filedialog
 import os
 import shutil
+from datetime import datetime, timedelta
+
 
 class ParentWindow(Frame):
     def __init__(self, master):
@@ -11,7 +13,7 @@ class ParentWindow(Frame):
         self.master.title("File Transfer")
 
         #Creates button to select files from source directory
-        self.sourceDir_btn = Button(text='Select Source', width=20)
+        self.sourceDir_btn = Button(text='Select Source', width=20, command=self.sourceDir)
         #Positions source button in gui using tkinter grid()
         self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
 
@@ -20,7 +22,7 @@ class ParentWindow(Frame):
         #Positions entry in GUI using tkinter grid() padx and pady are the same as the button to ensure they will line up
         self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))
 
-        self.destDir_btn = Button(text="Select Destination", width=20)
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
         #positions destination button in GUI using tkinter grid() on the next row under the source button
         self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15,10))
 
@@ -28,12 +30,6 @@ class ParentWindow(Frame):
         self.destination_dir = Entry(width=75)
         #Positions entry inGUI using tkinter grid() padx and pady are the same as the button to ensure they will line up
         self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10))
-
-        #Creates button to select files from source directory
-        self.sourceDir_btn = Button(text="Select Source", width=20, command=self.sourceDir)
-
-        #Creates button to select files from source directory
-        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
 
         #Creates button to transfer files
         self.transfer_btn = Button(text='Transfter Files', width=20, command=self.transferFiles)
@@ -66,17 +62,30 @@ class ParentWindow(Frame):
 
     #Creates function to transfer files from one directory to another
     def transferFiles(self):
+        #Gets the current time
+        now = datetime.now()
+        #Utilizing timedelta
+        oneDay = now - timedelta(hours=24)
+        
         #Gets source directory
         source = self.source_dir.get()
         #gets destination directory
         destination = self.destination_dir.get()
         #Gets a list of files in the source directory
         source_files = os.listdir(source)
+        
         #Runs through each file in the source directory
-        for i in source_files:
-            #Moves each file from the source to the destination
-            shutil.move(source + '/' + i, destination)
-        print(i + ' was succesfully transferred.')
+        for file in source_files:
+            #Getsa the full file path
+            file_path = os.path.join(source, file)
+            #gets the most recent modification time of a file
+            file_mod_time = datetime.fromtimestamp(os.path.getmtime(file_path))
+
+            #Cheacks if the file was modified in the last 24 hours
+            if file_mod_time > oneDay:
+                #Moves file to destinastion folder
+                shutil.move(file_path, destination)
+                print(file + "was transferred succesfully")
 
     
     #Creates function to exit program
